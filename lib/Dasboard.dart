19dd276d50cae334b96c21b35f1f706a4e7f46d1
irvanthole <irvanpartii@gmail.com>
2020-04-11 10:59:14 +0700

    penyesuaian dasboard page

--- lib/Dasboard.dart
+++ lib/Dasboard.dart
@@ -37,49 +37,50 @@ class _DasboardState extends State<Dasboard> {
         body: Column(
           children: <Widget>[
             Flexible(
-                flex: 1,
-                child: Material(
-                  elevation: 10.0,
-                  child: Container(
-                    color: Color(0xffB22222),
-                    child: Center(
-                      child:  Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Material(
-                              elevation: 15.0,
-                              borderRadius: BorderRadius.circular(50),
-                              color: Colors.transparent,
-                              child: CircleAvatar(
-                                radius: 50,
-                                backgroundColor: Colors.transparent,
-                                backgroundImage: AssetImage('img/irvan.png'),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 10),
-                              child: Text('IRVAN',
-                                  style: TextStyle(
-                                      fontSize: 30, color: Colors.white)),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(3.0),
-                              child: Text('201669040001',
-                                  style: TextStyle(
-                                      fontSize: 20, color: Colors.white)),
-                            ),
-                            Text('Informatika',
-                                style: TextStyle(
-                                    fontSize: 20, color: Colors.white)),
-                          ],
+              flex: 1,
+              child: Material(
+                elevation: 15.0,
+                child: Container(
+                  color: Color(0xffB22222),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Material(
+                          elevation: 15.0,
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.transparent,
+                          child: CircleAvatar(
+                            radius: 50,
+                            backgroundColor: Colors.transparent,
+                            backgroundImage: AssetImage('img/irvan.png'),
+                          ),
                         ),
-                      ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Text('IRVAN',
+                              style:
+                                  TextStyle(fontSize: 30, color: Colors.white)),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(3.0),
+                          child: Text('201669040001',
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.white)),
+                        ),
+                        Text('Informatika',
+                            style:
+                                TextStyle(fontSize: 20, color: Colors.white)),
+                      ],
                     ),
                   ),
                 ),
+              ),
+            ),
             Flexible(
               flex: 2,
               child: Container(
+                color: Color(0xffE9E9E9),
                 padding: EdgeInsets.all(20.0),
                 child: GridView.count(
                   crossAxisCount: 2,
@@ -119,17 +120,23 @@ class ListMenu extends StatelessWidget {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Icon(icon),
+                Icon(
+                  icon,
+                  color: Color(0xff4D4D4D),
+                ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Text(
                     text,
-                    style: TextStyle(fontSize: 17.0),
+                    style: TextStyle(fontSize: 17.0, color: Color(0xff4D4D4D)),
                   ),
                 )
               ],
             ),
-            Icon(Icons.arrow_right)
+            Icon(
+              Icons.arrow_right,
+              color: Color(0xff4D4D4D),
+            )
           ],
         ),
       ),
@@ -163,13 +170,13 @@ class MenuUtama extends StatelessWidget {
                 Icon(
                   icon,
                   size: 75,
-                  color: Colors.redAccent,
+                  color: Color(0xffB22222),
                 ),
                 Padding(
                   padding: const EdgeInsets.only(top: 10.0),
                   child: Text(
                     text,
-                    style: TextStyle(fontSize: 17.0),
+                    style: TextStyle(fontSize: 17.0, color: Color(0xff4D4D4D)),
                   ),
                 )
               ],
