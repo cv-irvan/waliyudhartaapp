2a5a63baf8576c4021e4b3f77ca4e2892994fc6d
irvanthole <irvanpartii@gmail.com>
2020-04-23 14:25:19 +0700

    menambah login page

---
+++ lib/login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage('img/Splashscreen.png'),fit: BoxFit.cover)
+        ),
+      )
+    );
+  }
+}
