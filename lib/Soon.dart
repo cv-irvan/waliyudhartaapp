d522dbbf4d9f96dc26393ff7e9aaad33cf11abdf
irvanthole <irvanpartii@gmail.com>
2020-06-24 22:57:11 +0700

    menambahkan comming soon

--- lib/Soon.dart
+++ lib/Soon.dart
@@ -10,13 +10,25 @@ class _SoonState extends State<Soon> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Lottie.asset(
-          'img/soon.json',
-          width: 200,
-          height: 200,
-          fit: BoxFit.fill,
-        ),
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: Container(
+              child: Lottie.asset(
+                'img/soon.json',
+                width: 300,
+                height: 300,
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Text('Coming', style: TextStyle(fontSize: 30, color: Colors.black)),
+              Text('Soon', style: TextStyle(fontSize: 30, color: Colors.red)),
+            ],
+          ),
+        ],
       ),
     );
   }
